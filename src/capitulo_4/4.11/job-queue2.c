512e09c29a70bda26678c843be76fffd19eb1eb5
guigapamplona <guigapamplona@gmail.com>
2021-08-17 17:23:15 -0400

    Se mejora el codigo y agrega caso de prueba

--- src/capitulo_4/4.11/job-queue2.c
+++ src/capitulo_4/4.11/job-queue2.c
@@ -4,7 +4,8 @@
 struct job { 
     /* Link field for linked list. */ 
     struct job* next; 
-    /* Other fields describing work to be done... */ 
+    /* Other fields describing work to be done... */
+    char file_name[40];
 }; 
 
 /* A linked list of pending jobs. */ 
@@ -16,8 +17,9 @@ pthread_mutex_t job_queue_mutex = PTHREAD_MUTEX_INITIALIZER;
 // Se crea una funcion donde se ejecuta el trabajo que realiza cada thread
 void* process_job (void* arg) 
 {
-    char* output_text = (char*) arg;
-    printf ("%s\n" , output_text);
+    struct job* current_job = (struct job*) arg;
+    sprintf(current_job->file_name, "thread_name:%ld" , (long)pthread_self()); 
+    printf ("%s\n" , current_job->file_name);
 
     return NULL;
 }
@@ -32,8 +34,9 @@ void* thread_function (void* arg)
         pthread_mutex_lock (&job_queue_mutex); 
 
         /* Now it's safe to check if the queue is empty. */ 
-        if (job_queue == NULL) 
+        if (job_queue == NULL) { 
             next_job = NULL; 
+        }
 
         else { 
             /* Get the next available job. */ 
@@ -60,13 +63,28 @@ void* thread_function (void* arg)
     return NULL; 
 }
 
-
+// Se crea main donde se prueba la lista de jobs e imprime cada thread su id
 int main ()
 {
+    pthread_t threads[20];
     int i;
 
+    struct job* prt_queue_head = (struct job*) malloc(sizeof(struct job));
+    struct job* prt_queue_next = prt_queue_head;
+
     for (i = 0; i < 20; i++) {
+        struct job* trabajo = (struct job*) malloc(sizeof(struct job));
+        prt_queue_next->next = trabajo;
+        prt_queue_next = trabajo;
     }
+
+    job_queue = prt_queue_head;
+
+    for (i = 0; i < 20; i++) 
+        pthread_create(&(threads[i]), NULL, thread_function, NULL);
+    
+    for (i = 0; i < 20; ++i) 
+        pthread_join (threads[i], NULL);
 
     return 0;
 }
