ca7a357ec8839c34943c45929281e7044e82d31c
frandepy2 <frandepy@fpuna.edu.py>
2021-09-13 13:42:01 -0400

    Se agrego caso por defecto listing 5.6

--- src/capitulo_5/5.6/mmap-read.c
+++ src/capitulo_5/5.6/mmap-read.c
@@ -12,7 +12,13 @@ int main(int argc, char * const argv[]){
     int integer;
 
     /*Open the file */
-    fd = open(argv[1], O_RDWR, S_IRUSR | S_IWUSR);
+    if (argv[1]!=NULL){
+        printf("Usage: %s <file>\n", argv[1]);
+        fd = open(argv[1], O_RDWR, S_IRUSR | S_IWUSR);
+    }else{
+        printf("Usage: /tmp/integer-file\n");
+        fd = open("/tmp/integer-file", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    }
 
     /*Create a memory mapping*/
     file_memory = mmap(0,FILE_LENGTH, PROT_READ | PROT_WRITE, MAP_SHARED, fd ,0);
