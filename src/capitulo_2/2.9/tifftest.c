ac02172380cd9631c92e22a9f1f5e818fa36b779
frandepy2 <frandepy@fpuna.edu.py>
2021-09-13 13:19:29 -0400

    cambiamos en listing 2.9 el argv por un path fijo

--- src/capitulo_2/2.9/tifftest.c
+++ src/capitulo_2/2.9/tifftest.c
@@ -2,8 +2,11 @@
 #include "tiffio.h"
 
 int main(int argc, char **argv){
+
+    printf("%s\n", "el archivo actual en el libro se introduce con un args \npero para comodidad de la prueba forzamos a un path incluido dogecoin.tiff");
+
     TIFF * tiff;
-    tiff = TIFFOpen(argv[1], "r");
+    tiff = TIFFOpen("./src/capitulo_2/2.9/dogecoin.tiff", "r");
     
     if(tiff == NULL){
         printf("Error abriendo el archivo\n");
